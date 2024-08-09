02390117b80f7a33f26d07484cd6167bf44505d4
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-08-09 10:46:53 +0330

    configuration models
    i  have configed models for my blog

--- config/models.py
+++ config/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=100 , null=True)
+    time_created = models.TimeField(auto_now=True , null=False)
+    description = models.CharField(max_length=250 ,null=True)
+    # author = models.ForeignKey(User ,on_delete=models.CASCADE , null=False)
+    body = models.CharField(max_length=10000 , null=False)
+    
