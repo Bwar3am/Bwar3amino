7d4f15265f78917a2929f5e57426a4c5ce799956
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-08-10 18:17:20 +0330

    models
    ive updated model and add some rows to that!!

--- config/models.py
+++ config/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class blog(models.Model):
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=100 , null=True)
     time_created = models.TimeField(auto_now=True , null=False)
     description = models.CharField(max_length=250 ,null=True)
-    # author = models.ForeignKey(User ,on_delete=models.CASCADE , null=False)
     body = models.CharField(max_length=10000 , null=False)
+    image = models.ImageField(upload_to='pics/' , default="pics/placeholder.png" , null=True)
+    
+    def __str__(self):
+        return  self.title
     
