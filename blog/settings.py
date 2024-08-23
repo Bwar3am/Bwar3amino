e8214ab4d05711003dcb685075db38b5680a47e9
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-08-23 10:47:18 +0330

    created  authuntication system
    ive created authuntication system and currently it works good.

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'config',
+    'widget_tweaks',
     'accounts',
 ]
 
@@ -130,3 +131,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/" 
