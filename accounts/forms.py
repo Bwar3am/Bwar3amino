264e478245c5512d7240488f32e2369ac29e55b4
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-08-25 08:54:50 +0330

    maked model
    maked model

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+# from django import forms
+# from . import models
+
+
+# class userprofileform(forms.ModelForm):
+#     class Meta:
+#         model = profile
+        
--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-# class profile (models.Model):
-#     name =
-#     last_name = 
-#     email_address = 
-#     profile_image = 
-    
+class profile (models.Model):
+    name = models.CharField(max_length=20 , null=False)
+    last_name = models.CharField(max_length=20 , null=False)
+    email_address = models.EmailField(null=False)
+    profile_image = models.ImageField(null=True)
+    bio = models.CharField(null=False , max_length=300)
+    # about = models.CharField(null=False , max_length=100)
     
     
