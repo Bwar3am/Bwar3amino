cf5c397fecfd5b6936115f17a05a814511906994
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-09-04 19:14:06 +0330

    udpdated interface
    udpdated interface

--- config/views.py
+++ config/views.py
@@ -7,13 +7,16 @@ from accounts.models import userinfo
 
 
 def home(request):
+    count = blog.objects.all().count()
+    total_views = blog.objects.aggregate(total_views=models.Sum('view_count'))['total_views']
+    totalauthors = User.objects.filter(is_superuser=True).count()
     mvp= blog.objects.order_by('-view_count').first()
     fvp = blog.objects.order_by("view_count").first()
     info = blog.objects.all()
     pagechanger = Paginator(info , 4)
     page_number = request.GET.get("page")
     page_obj = pagechanger.get_page(page_number)
-    context = {"info":info , "page_obj":page_obj , "mvp" : mvp , "fvp":fvp }
+    context = {"info":info , "page_obj":page_obj , "mvp" : mvp , "fvp":fvp , "count":count  ,'total_views': total_views , "totalauthors":totalauthors} 
     return render(request , "index1.html" , context)
 
 
