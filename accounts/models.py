bcf60e137cbb9f69e4bcbd80f0e88be06f091275
Barsam Hassanzadeh-aski <141170967+Bwar3am@users.noreply.github.com>
2024-11-01 10:46:21 +0330

    removed some useless codes
    removed some useless codes

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
-
-# class users(models.Model):
-#     name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     bio = models.CharField(max_length=500)
-#     profile_image = models.ImageField(upload_to="pis/" , blank=True , null=True) 
-    
-    
-#     def __str__(self):
-#         return self.name   
 
 
 
